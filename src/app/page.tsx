947313046dbb01b8ee00c08dc0be4a58c20b7aed
victor <victoratnz@outlook.com>
2024-09-29 23:07:23 +1300

    Refactor layout component and add new components
    - Refactor layout component to include new components: AddPost, Feed, LeftMenu, RightMenu, and Stories.
    - Update Homepage to use the new components in a flex layout.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,27 @@
+import AddPost from "@/components/AddPost"
+import Feed from "@/components/Feed"
+import LeftMenu from "@/components/LeftMenu"
+import RightMenu from "@/components/RightMenu"
+import Stories from "@/components/Stories"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div className='flex gap-6'>
+      {/* LEFT */}
+      <div className="bg-gray-300 hidden xl:block w-[20%]">
+        <LeftMenu />
+      </div>
+      {/* CENTER */}
+      <div className="bg-red-300 w-full lg:w-[70%] xl:w-[50%]">
+        <Stories />
+        <AddPost />
+        <Feed />
+      </div>
+      {/* RIGHT */}
+      <div className="bg-blue-300 hidden lg:block w-[30%]">
+        <RightMenu />
+      </div>
+    </div>
   )
 }
 
