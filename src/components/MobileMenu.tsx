285d1575003cb290dd5aa6410b60cc2a4661dc29
victor <victoratnz@outlook.com>
2024-09-29 18:09:45 +1300

    Refactor MobileMenu component to hide on larger screens

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className=''>
+    <div className='md:hidden'>
       <div className='flex flex-col gap-[4.5px] cursor-pointer' onClick={()=>setIsOpen(pre=>!pre)}>
         <div className={`bg-blue-500 w-6 h-1 rounded-sm ${isOpen?"rotate-45":""} origin-left duration-500`}></div>
         <div className={`bg-blue-500 w-6 h-1 rounded-sm ${isOpen?"opacity-0":""} duration-500`}></div>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,39 @@
 import Link from 'next/link'
 import React from 'react'
 import MobileMenu from './MobileMenu'
+import Image from 'next/image'
 
 const Navbar = () => {
   return (
     <div className='flex items-center justify-between h-24'>
-        {/* LEFT */}
-        <div className='font-bold text-xl text-blue-600'>
-            <Link href="/">VICTORSOCIAL</Link>
+      {/* LEFT */}
+      <div className='md:hidden lg:block w-[20%]'>
+        <Link className='font-bold text-xl text-blue-600' href="/">
+          VICTORSOCIAL
+        </Link>
+      </div>
+      {/* CENTER */}
+      <div className='hidden md:flex w-[50%] text-sm'>
+        {/* Links */}
+        <div className='flex gap-6 text-gray-600'>
+          <Link href="/" className='flex gap-1 items-center'>
+            <Image src="/home.png" alt='Home' width={16} height={16} className='w-4 h-4'/>
+            <div>HomePage</div>
+          </Link>
+          <Link href="/" className='flex gap-1 items-center'>
+            <Image src="/friends.png" alt='Friends' width={16} height={16} className='w-4 h-4'/>
+            <div>Friends</div>
+          </Link>
+          <Link href="/" className='flex gap-1 items-center'>
+            <Image src="/stories.png" alt='Stories' width={16} height={16} className='w-4 h-4'/>
+            <div>Stories</div>
+          </Link>
         </div>
-        {/* CENTER */}
-        <div className='hidden'></div>
-        {/* RIGHT */}
-        <div className=''>
-          <MobileMenu />
-        </div>
+      </div>
+      {/* RIGHT */}
+      <div className='w-[30%] flex items-center justify-end gap-4 xl:gap-8'>
+        <MobileMenu />
+      </div>
     </div>
   )
 }
