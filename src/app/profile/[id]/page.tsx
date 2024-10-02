5d0af62f0048a14129bea9764ac9f7610c1da632
victor <victoratnz@outlook.com>
2024-10-02 20:54:04 +1300

    Refactor ProfilePage component to include LeftMenu, Feed, and RightMenu components

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,27 @@
+import Feed from '@/components/Feed'
+import LeftMenu from '@/components/LeftMenu'
+import RightMenu from '@/components/RightMenu'
 import React from 'react'
 
 const ProfilePage = () => {
-  return (
-    <div>ProfilePage</div>
-  )
+    return (
+        <div className='flex gap-6 pt-6'>
+            {/* LEFT */}
+            <div className=" hidden xl:block w-[20%]">
+                <LeftMenu />
+            </div>
+            {/* CENTER */}
+            <div className=" w-full lg:w-[70%] xl:w-[50%] ">
+                <div className="flex flex-col gap-6">
+                    <Feed />
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className=" hidden lg:block w-[30%]">
+                <RightMenu userId='12345'/>
+            </div>
+        </div>
+    )
 }
 
 export default ProfilePage
