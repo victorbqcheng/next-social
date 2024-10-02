ec45bd6f0122002ee091b2c630c849db1f669591
victor <victoratnz@outlook.com>
2024-10-02 20:54:19 +1300

    Add UserInfoCard and UserMediaCard components

---
+++ src/components/UserMediaCard.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserMediaCard = () => {
+    return (
+        <div className='p-4 bg-white shadow-md rounded-lg text-sm'>
+            UserMediaCard
+        </div>
+    )
+}
+
+export default UserMediaCard
