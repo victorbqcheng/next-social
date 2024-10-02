e623684005f185767d6555db7631ebd870ec51f5
victor <victoratnz@outlook.com>
2024-10-02 23:17:41 +1300

    Refactor UserMediaCard component to include user media and improve styling

--- src/components/UserMediaCard.tsx
+++ src/components/UserMediaCard.tsx
@@ -1,9 +1,45 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
-const UserMediaCard = () => {
+type UserMediaCardProps = {
+    userId?:string;
+};
+
+const UserMediaCard = ({userId}:UserMediaCardProps) => {
     return (
-        <div className='p-4 bg-white shadow-md rounded-lg text-sm'>
-            UserMediaCard
+        <div className='p-4 bg-white shadow-md rounded-lg text-sm flex flex-col gap-4'>
+            {/* TOP */}
+            <div className='flex items-center justify-between font-medium'>
+                <span className='text-gray-500'>User Media</span>
+                <Link href='/' className='text-xs text-blue-500'>see all</Link>
+            </div>
+            {/* BOTTOM */}
+            <div className='flex justify-between flex-wrap gap-2 w-full relative'>
+                
+                <div className='w-1/5 relative h-24'>
+                    <Image src="https://images.pexels.com/photos/10509041/pexels-photo-10509041.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=''
+                           fill className='rounded-md object-cover'/>
+                </div>
+                
+                <div className='w-1/5 relative h-24'>
+                    <Image src="https://images.pexels.com/photos/10509041/pexels-photo-10509041.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=''
+                           fill className='rounded-md object-cover'/>
+                </div>
+                <div className='w-1/5 relative h-24'>
+                    <Image src="https://images.pexels.com/photos/10509041/pexels-photo-10509041.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=''
+                           fill className='rounded-md object-cover'/>
+                </div>
+                <div className='w-1/5 relative h-24'>
+                    <Image src="https://images.pexels.com/photos/10509041/pexels-photo-10509041.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=''
+                           fill className='rounded-md object-cover'/>
+                </div>
+                <div className='w-1/5 relative h-24'>
+                    <Image src="https://images.pexels.com/photos/10509041/pexels-photo-10509041.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=''
+                           fill className='rounded-md object-cover'/>
+                </div>
+                
+            </div>
         </div>
     )
 }
