6b67dbad2c469c440d9b686f3e2493c761b7f658
victor <victoratnz@outlook.com>
2024-10-08 22:57:33 +1300

    Refactor ProfileCard component to fetch user data from the database and display it dynamically

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isProtectedRoute = createRouteMatcher(["settings(.*)", ""]);
+const isProtectedRoute = createRouteMatcher(["settings(.*)", "/"]);
 
 export default clerkMiddleware((auth, request)=>{
     if(isProtectedRoute(request))
