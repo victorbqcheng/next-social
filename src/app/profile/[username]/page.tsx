8507db2513cd58b9be475b56178a28429a95773b
victor <victoratnz@outlook.com>
2024-10-08 23:45:33 +1300

    Refactor ProfilePage component to dynamically fetch and display user data

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -2,7 +2,10 @@ import Feed from '@/components/Feed'
 import LeftMenu from '@/components/LeftMenu'
 import RightMenu from '@/components/RightMenu'
 import prisma from '@/lib/client'
+import { auth } from '@clerk/nextjs/server'
+import { User } from '@prisma/client'
 import Image from 'next/image'
+import { notFound } from 'next/navigation'
 import React from 'react'
 
 type ProfilePageProps = {
@@ -16,9 +19,32 @@ const ProfilePage = async ({params}:ProfilePageProps) => {
     const user = await prisma.user.findFirst({
         where:{
             username
+        },
+        include:{
+            _count:{
+                select:{
+                    followers: true,
+                    followings: true,
+                    posts: true
+                }
+            }
         }
     });
-    
+    if(!user) return notFound();
+
+    const {userId:currentUserId} = auth();
+    let isBlocked = false;
+    if(currentUserId){
+        const blockRequest = await prisma.blockRequest.findFirst({
+            where:{
+                senderId: user.id,
+                receiverId: currentUserId!
+            }
+        });
+        if(blockRequest) isBlocked = true;
+    }
+    if(isBlocked) return notFound();
+
     return (
         <div className='flex gap-6 pt-6'>
             {/* LEFT */}
@@ -28,39 +54,49 @@ const ProfilePage = async ({params}:ProfilePageProps) => {
             {/* CENTER */}
             <div className=" w-full lg:w-[70%] xl:w-[50%] ">
                 <div className="flex flex-col gap-6">
-                    <ProfilepageCard />
+                    <ProfilepageCard user={user}/>
                     <Feed />
                 </div>
             </div>
             {/* RIGHT */}
             <div className=" hidden lg:block w-[30%]">
-                <RightMenu userId='12345' />
+                <RightMenu user={user} />
             </div>
         </div>
     )
 }
 
-const ProfilepageCard = () => {
+type ProfilepageCardProps = {
+    user: User & {
+        _count:{
+            followers: number,
+            followings: number,
+            posts: number
+        }
+    }
+};
+
+const ProfilepageCard = ({user}:ProfilepageCardProps) => {
     return (
         <div className='flex flex-col gap-5 items-center'>
             <div className='relative h-64 w-full'>
-                <Image src="https://images.pexels.com/photos/14266208/pexels-photo-14266208.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=""
+                <Image src={user.cover || "/nocover.png"} alt=""
                     fill className='object-cover' />
-                <Image src="https://images.pexels.com/photos/28647129/pexels-photo-28647129/free-photo-of-woman-in-white-dress-with-floral-wreath-outdoors.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load" alt=""
+                <Image src={user.avatar || "/noavatar.png"} alt=""
                     width={112} height={112} className='absolute rounded-full ring-white ring-2 w-28 h-28 object-cover left-0 right-0 m-auto -bottom-14' />
             </div>
-            <span className='mt-12 font-semibold text-xl'>Joe Jones</span>
+            <span className='mt-12 font-semibold text-xl'>{(user.name&&user.surname)?user.name + " "+ user.surname:user.username}</span>
             <div className='flex gap-8 text-sm'>
                 <div className='flex flex-col items-center'>
-                    <span className='font-semibold'>142</span>
+                    <span className='font-semibold'>{user._count.posts}</span>
                     <span className='text-gray-800'>Posts</span>
                 </div>
                 <div className='flex flex-col items-center'>
-                    <span className='font-semibold'>1.2K</span>
+                    <span className='font-semibold'>{user._count.followers}</span>
                     <span className='text-gray-800'>Followers</span>
                 </div>
                 <div className='flex flex-col items-center'>
-                    <span className='font-semibold'>1.4K</span>
+                    <span className='font-semibold'>{user._count.followings}</span>
                     <span className='text-gray-800'>Following</span>
                 </div>
             </div>
