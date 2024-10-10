b23418a2d009060b44dcaa7c246228aab6bfb6b4
victor <victoratnz@outlook.com>
2024-10-10 23:54:04 +1300

    Refactor UserInfoCardInteraction component to dynamically handle user interactions

--- src/components/rightMenu/UserInfoCardInteraction.tsx
+++ src/components/rightMenu/UserInfoCardInteraction.tsx
@@ -1,11 +1,14 @@
+"use client"
+
+import { switchBlock, switchFollow } from '@/lib/actions';
 import React from 'react'
 
 type UserInfoCardInteractionProps = {
-    userId:string;
-    currentUserId:string;
-    isUserBlocked:boolean;
-    isFollowing:boolean;
-    isFollowingSent:boolean;
+    userId: string;
+    currentUserId: string;
+    isUserBlocked: boolean;
+    isFollowing: boolean;
+    isFollowingSent: boolean;
 };
 
 const UserInfoCardInteraction = ({
@@ -14,17 +17,57 @@ const UserInfoCardInteraction = ({
     isUserBlocked,
     isFollowing,
     isFollowingSent
-}:UserInfoCardInteractionProps) => {
-  return (
-    <>
-        <button className='bg-blue-600 text-white p-2 rounded-md'>
-            {isFollowing?"Unfollow":isFollowingSent?"Friend request sent":"Follow"}
-        </button>
-        <span className='text-red-400 text-xs cursor-pointer self-end'>
-            {isUserBlocked?"Unblock User":"Block User"}
-        </span>
-    </>
-  )
+}: UserInfoCardInteractionProps) => {
+    console.log("entered UserInfoCardInteraction");
+    const [useState, setUserState] = React.useState({
+        following: isFollowing,
+        blocked: isUserBlocked,
+        followingRequestSent: isFollowingSent
+    });
+
+    const follow = async () => {
+        try {
+            await switchFollow(userId);
+            setUserState(pre => (
+                {
+                    ...pre,
+                    following: pre.following && false,
+                    followingRequestSent: (!pre.following && !pre.followingRequestSent) ? true : false
+                }
+            ));
+        } catch (error) {
+
+        }
+    };
+
+    const block = async () => {
+        try {
+            await switchBlock(userId);
+            setUserState(pre => ({
+                ...pre,
+                blocked: !pre.blocked
+            }));
+        } catch (error) {
+
+        }
+    };
+
+    return (
+        <>
+            <form action={follow}>
+                <button className='w-full bg-blue-600 text-white p-2 rounded-md'>
+                    {useState.following ? "Unfollow" : useState.followingRequestSent ? "Friend request sent" : "Follow"}
+                </button>
+            </form>
+            <form action={block} className='self-end'>
+                <button type='submit'>
+                    <span className='text-red-400 text-xs cursor-pointer self-end'>
+                        {useState.blocked ? "Unblock User" : "Block User"}
+                    </span>
+                </button>
+            </form>
+        </>
+    )
 }
 
 export default UserInfoCardInteraction
